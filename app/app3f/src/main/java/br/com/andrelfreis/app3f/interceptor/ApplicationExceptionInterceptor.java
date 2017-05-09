ee73b300935789ba6820ac16cd71bc4fb657918c
André Reis <arghos[at]gmail.com>
2017-05-08 22:51:29 -0300

    iniciando tratamento de excecoes

--- app/app3f/src/main/java/br/com/andrelfreis/app3f/interceptor/ApplicationExceptionInterceptor.java
+++ app/app3f/src/main/java/br/com/andrelfreis/app3f/interceptor/ApplicationExceptionInterceptor.java
@@ -1,12 +1,16 @@
 package br.com.andrelfreis.app3f.interceptor;
 
+
+import static com.google.common.base.Throwables.getRootCause;
+
 import javax.enterprise.context.RequestScoped;
 
 import br.com.caelum.vraptor.AroundCall;
 import br.com.caelum.vraptor.Intercepts;
+import br.com.caelum.vraptor.interceptor.ExceptionHandlerInterceptor;
 import br.com.caelum.vraptor.interceptor.SimpleInterceptorStack;
 
-@Intercepts
+@Intercepts(before=ExceptionHandlerInterceptor.class)
 @RequestScoped
 public class ApplicationExceptionInterceptor {
 
@@ -17,6 +21,9 @@ public class ApplicationExceptionInterceptor {
 			stack.next();
 		} 
 		catch (Exception e) {
+			
+			Throwable rootCause = getRootCause(e);
+						
 			System.out.println("\n\n\t ********* Teste Exception Interceptor  ********** \n\n");
 			throw e;
 		}
